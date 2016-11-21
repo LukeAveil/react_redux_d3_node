fa968fc6ec24d4dbdfb1cce6bbefbd6521793ad3
Luke <lukeaveil@yahoo.com>
2016-11-21 16:04:35 +0000

    adds setstate functionailty

--- react_basics/public/app.jsx
+++ react_basics/public/app.jsx
@@ -4,13 +4,33 @@ var Greeter = React.createClass({
       name: "React"
     };
   },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    this.refs.name.value = "";
+
+    this.setState({
+      name: name
+    });
+  },
   render: function() {
-    var name = this.props.name;
+    var name = this.state.name;
 
     return (
       <div>
         <h1>Hello {name}</h1>
         <p>This is from a component</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
