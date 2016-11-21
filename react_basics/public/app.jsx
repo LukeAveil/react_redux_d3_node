820cfa85f673f28b8b96f3f804bf80ffe0eddd11
Luke <lukeaveil@yahoo.com>
2016-11-21 16:47:11 +0000

    seperates out nested components

--- react_basics/public/app.jsx
+++ react_basics/public/app.jsx
@@ -1,3 +1,35 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = "";
+      this.props.onNewName(name);
+    }
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
@@ -9,32 +41,18 @@ var Greeter = React.createClass({
       name: this.props.name
     };
   },
-  onButtonClick: function(e) {
-    e.preventDefault();
-
-    var nameRef = this.refs.name;
-
-    var name = nameRef.value;
-    nameRef.value = "";
-
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function(name) {
+    this.setState({
+      name: name
+    });
   },
   render: function() {
     var name = this.state.name;
 
     return (
       <div>
-        <h1>Hello {name}</h1>
-        <p>This is from a component</p>
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage name={name}/>
+        <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
   }
