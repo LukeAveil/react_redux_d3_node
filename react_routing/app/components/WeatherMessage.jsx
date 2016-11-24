1d072fe71104ec83ab513b8d99c4db5ee0042d2a
Luke <lukeaveil@yahoo.com>
2016-11-24 17:47:05 +0000

    adds setup for weather message and weather form components

---
+++ react_routing/app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+  render: function() {
+    return (
+      <h3>7 Degrees in London</h3>
+    );
+  }
+});
+
+module.exports = WeatherMessage;
