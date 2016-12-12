7df49535793bb2798a2d11667a711e9ee3497faa
Luke <lukeaveil@yahoo.com>
2016-12-12 16:25:15 +0000

    adds components, passes props down and creates todo components with unique key

--- react_todo/app/components/TodoApp.jsx
+++ react_todo/app/components/TodoApp.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Feed the cat'
+        }, {
+          id: 3,
+          text: 'Water the plants'
+        }, {
+          id: 4,
+          text: 'Make the bed'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
