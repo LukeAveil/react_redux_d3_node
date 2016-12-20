7e694197330421a009ba69e44ed0636b3c55f70c
Luke <lukeaveil@yahoo.com>
2016-12-20 13:38:53 +0000

    adds functionality to filter by search text and sort todos with non-completed first

--- react_todo/public/bundle.js
+++ react_todo/public/bundle.js
@@ -34050,6 +34050,23 @@
 	      return !todo.completed || showCompleted;
 	    });
 
+	    // Filter by searchText
+	    filteredTodos = filteredTodos.filter(function (todo) {
+	      var text = todo.text.toLowerCase();
+	      return searchText.length === 0 || text.indexOf(searchText) > -1;
+	    });
+
+	    // Sort todos with non-completed first
+	    filteredTodos.sort(function (a, b) {
+	      if (!a.completed && b.completed) {
+	        return -1;
+	      } else if (a.completed && !b.completed) {
+	        return 1;
+	      } else {
+	        return 0;
+	      }
+	    });
+
 	    return filteredTodos;
 	  }
 	};
