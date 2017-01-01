862128eb1a366e7e02373f066b852ad5f4a4b8b2
Luke <lukeaveil@yahoo.com>
2017-01-01 15:47:36 +0000

    adds examples of adding to arrays

--- learn_redux/app/redux-example.jsx
+++ learn_redux/app/redux-example.jsx
@@ -2,13 +2,46 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   switch(action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
       };
+      case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+            ...state.hobbies,
+            {
+              id: nextHobbyId++,
+              hobby: action.hobby
+            }
+          ]
+        };
+        case 'ADD_MOVIE':
+          return {
+            ...state,
+            movies: [
+              ...state.movies,
+              {
+                id: nextMovieId++,
+                movie: action.movie,
+                genre: action.genre
+              }
+            ]
+          };
       default:
         return state;
   }
@@ -22,14 +55,31 @@ store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is', state.name);
-});
+  document.getElementById('app').innerHTML = state.name;
 
-var currentState = store.getState();
-console.log('current state', currentState);
+  console.log('New state', store.getState());
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Luke'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Cycling'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Matrix',
+  genre: 'Sc-iFi'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Titanic',
+  genre: 'Romance'
 });
 
 store.dispatch({
