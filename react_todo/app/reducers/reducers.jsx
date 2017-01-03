81695acd0bbd66f6585dfdd24521e0593bd441a4
Luke <lukeaveil@yahoo.com>
2017-01-03 15:18:18 +0000

    adds todo reducer

--- react_todo/app/reducers/reducers.jsx
+++ react_todo/app/reducers/reducers.jsx
@@ -1,3 +1,5 @@
+var uuid = require('node-uuid');
+
 export var searchTextReducer = (state = '', action) => {
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
@@ -11,6 +13,33 @@ export var showCompletedReducer = (state = false, action) => {
   switch(action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
+    default:
+      return state;
+  };
+};
+
+export var todosReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+
+          return {
+            ...state,
+            completed: nextCompleted
+          }
+        }
+      });
     default:
       return state;
   };
